c0273e6109ff7f8c91879add49fbd8fc7b305fa7
Vladimir Kolosov <12vova1212ukr@gmail.com>
2026-03-06 18:02:01 +0200

    not ready yet

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const menu = document.querySelector('.menu');
+  const openBtn = document.querySelector('.icon--menu');
+  const closeBtn = document.querySelector('.icon--close');
+
+  openBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    menu.classList.add('menu--open');
+    document.body.classList.add('no-scroll');
+  });
+
+  closeBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    menu.classList.remove('menu--open');
+    document.body.classList.remove('no-scroll');
+  });
+});
+
