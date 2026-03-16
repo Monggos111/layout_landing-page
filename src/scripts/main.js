2c6df77e666693ce405461edce29c220811409f7
Vladimir Kolosov <12vova1212ukr@gmail.com>
2026-03-16 18:24:37 +0200

    changed buttons, menus and inputs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,9 @@
 'use strict';
-
 document.addEventListener('DOMContentLoaded', () => {
   const menu = document.querySelector('.menu');
   const openBtn = document.querySelector('.icon--menu');
   const closeBtn = document.querySelector('.icon--close');
+  const menuLinks = document.querySelectorAll('.menu__link');
 
   openBtn.addEventListener('click', (e) => {
     e.preventDefault();
@@ -13,8 +13,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
   closeBtn.addEventListener('click', (e) => {
     e.preventDefault();
+    closeMenu();
+  });
+
+  menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      closeMenu();
+    });
+  });
+
+  function closeMenu() {
     menu.classList.remove('menu--open');
     document.body.classList.remove('no-scroll');
-  });
+  }
 });
-
